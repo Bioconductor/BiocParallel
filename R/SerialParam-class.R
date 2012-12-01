9ff0bbe91b7acb4eb0bd74a07f97cfa8217dd29e
Martin Morgan <mtmorgan@fhcrc.org>
2012-12-01 15:46:36 +0000

    Make SerialParam a singleton class
    From: Ryan C. Thompson <rct@thompsonclan.org>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@71593 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/SerialParam-class.R
+++ R/SerialParam-class.R
@@ -1,7 +1,10 @@
-SerialParam <- setClass("SerialParam",
+.SerialParam <- setClass("SerialParam",
     representation(),
     prototype(),
     contains = "BiocParallelParam")
+
+.SerialParamSingleton <- .SerialParam()
+SerialParam <- function() .SerialParamSingleton
 
 ## methods are usually implemented as c("ANY", "ANY", "ANY") in
 ## corresponding generic.R file
