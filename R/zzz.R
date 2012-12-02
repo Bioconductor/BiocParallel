00957621a4546b24d3ecad7e95b4414615642cc6
Martin Morgan <mtmorgan@fhcrc.org>
2012-12-02 11:14:51 -0800

    default SnowParam has non-zero cores

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,8 @@
-splitIndices <- parallel::splitIndices
-
 .onLoad <-
     function(libname, pkgname)
 {
     ## only SnowParam on widows, which is then the default (first)
-    register(getOption("SnowParam", SnowParam(spec=0L)))
+    register(getOption("SnowParam", SnowParam(workers=detectCores())))
     if (.Platform$OS.type != "windows")
         register(getOption("MulticoreParam", MulticoreParam()))
 }
