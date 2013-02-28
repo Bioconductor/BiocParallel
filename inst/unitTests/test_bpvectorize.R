aa55b80557ea8a7ece4d063510a59400421dbe7a
mtmorgan@fhcrc.org <mtmorgan@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-02-28 01:44:53 +0000

    merge github changes
    - 752f68d Martin Morgan: avoid man page link to parallel::mcparallel
    - 41f0947 Martin Morgan: windows isChild alias required
    - 39655ca Martin Morgan: make unit tests OS-dependent
    - c94dac7 Martin Morgan: man page example typos
    - 1a8bfd9 Martin Morgan: condition import from parallel
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@73811 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_bpvectorize.R
+++ inst/unitTests/test_bpvectorize.R
@@ -1,14 +1,32 @@
-library(foreach)
 library(doParallel)
-registerDoParallel(cores=2)
-params <- list(mc=MulticoreParam(2),
-               snow=SnowParam(type="FORK", workers=2),
-               dopar=DoparParam())
 
-x <- 1:10
-expected <- sqrt(x)
-for (ptype in names(params)) {
-    psqrt <- bpvectorize(sqrt, BPPARAM=params[[ptype]])
-    checkIdentical(expected, psqrt(x),
-                   paste(ptype, "BPPARAM works with bplapply"))
+.fork_not_windows <- function(expected, expr)
+{
+    err <- NULL
+    obs <- tryCatch(expr, error=function(e) {
+        if (!all(grepl("fork clusters are not supported on Windows",
+                       conditionMessage(e))))
+            err <<- conditionMessage(e)
+        expected
+    })
+    checkTrue(is.null(err))
+    checkIdentical(expected, obs)
+}
+
+test_bpvectorize_Params <- function()
+{
+    params <- list(serial=SerialParam(),
+                   mc=MulticoreParam(2),
+                   snow0=SnowParam(2, "FORK"),
+                   snow1=SnowParam(2, "PSOCK"),
+                   dopar=DoparParam())
+
+    dop <- registerDoParallel(cores=2)
+
+    x <- 1:10
+    expected <- sqrt(x)
+    for (ptype in names(params)) {
+        psqrt <- bpvectorize(sqrt, BPPARAM=params[[ptype]])
+        .fork_not_windows(expected, psqrt(x))
+    }
 }
