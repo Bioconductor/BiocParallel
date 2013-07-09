396934b610e22048a885f95eb3108e6235f6edcd
mllg <michellang@gmail.com>
2013-07-09 15:40:19 +0200

    error handling for dopar

--- R/DoparParam-class.R
+++ R/DoparParam-class.R
@@ -1,7 +1,11 @@
-.DoparParamSingleton <- setRefClass("DoparParam",
-    contains="BiocParallelParam")$new()
+.DoparParam <- setRefClass("DoparParam",
+    contains="BiocParallelParam",
+    fields=list(),
+)
 
-DoparParam <- function() .DoparParamSingleton
+DoparParam <- function(catch.errors) {
+  .DoparParam(catch.errors = catch.errors)
+}
 
 ## control
 
@@ -13,12 +17,9 @@ setMethod(bpworkers, "DoparParam",
     else 0L
 })
 
-setMethod(bpisup, "DoparParam",
-    function(x, ...)
-{
-    ("package:foreach" %in% search()) &&
-        getDoParRegistered() && (getDoParName() != "doSEQ") &&
-            getDoParWorkers() > 1L
+setMethod(bpisup, "DoparParam", function(x, ...) {
+    "package:foreach" %in% search() && getDoParRegistered() &&
+      getDoParName() != "doSEQ" && getDoParWorkers() > 1L
 })
 
 ## evaluation
@@ -33,6 +34,12 @@ setMethod(bplapply, c("ANY", "DoparParam"),
         return(bplapply(X, FUN, ..., BPPARAM=SerialParam()))
 
     x <- NULL                           # quieten R CMD check
-    ans <- foreach(x=X) %dopar% FUN(x, ...)
-    setNames(ans, names(X))
+    results <- foreach(x=X, .errorhandling = "pass") %dopar% FUN(x, ...)
+    is.error = vapply(results, inherits, logical(1L), what = "error")
+    if (any(is.error)) {
+      if (BPPARAM$catch.errors)
+        LastError$store(obj = X, results = results, is.error = is.error, throw.error = TRUE)
+      stop(results[[head(which(is.error), 1L)]])
+    }
+    return(results)
 })
