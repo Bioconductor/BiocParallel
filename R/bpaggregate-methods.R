e910d58ac33802831f4e8a0558f35c0125290a90
mllg <michellang@gmail.com>
2013-09-23 12:09:15 +0200

    some doc changes

--- R/bpaggregate-methods.R
+++ R/bpaggregate-methods.R
@@ -77,3 +77,65 @@ setMethod("bpaggregate", c("formula", "BiocParallelParam"),
   }
 )
 
+
+function (x, by, FUN, ..., simplify = TRUE) 
+{
+    if (!is.data.frame(x)) 
+        x <- as.data.frame(x)
+    FUN <- match.fun(FUN)
+    if (NROW(x) == 0L) 
+        stop("no rows to aggregate")
+    if (NCOL(x) == 0L) {
+        x <- data.frame(x = rep(1, NROW(x)))
+        return(aggregate.data.frame(x, by, function(x) 0L)[seq_along(by)])
+    }
+    if (!is.list(by)) 
+        stop("'by' must be a list")
+    if (is.null(names(by)) && length(by)) 
+        names(by) <- paste("Group", seq_along(by), sep = ".")
+    else {
+        nam <- names(by)
+        ind <- which(!nzchar(nam))
+        names(by)[ind] <- paste("Group", ind, sep = ".")
+    }
+    nrx <- NROW(x)
+    grp <- double(nrx)
+    for (ind in rev(by)) {
+        if (length(ind) != nrx) 
+            stop("arguments must have same length")
+        ind <- as.factor(ind)
+        grp <- grp * nlevels(ind) + (as.integer(ind) - 1L)
+    }
+    y <- as.data.frame(by, stringsAsFactors = FALSE)
+    y <- y[match(sort(unique(grp)), grp, 0L), , drop = FALSE]
+    nry <- NROW(y)
+    z <- lapply(x, function(e) {
+        ans <- lapply(X = split(e, grp), FUN = FUN, ...)
+        if (simplify && length(len <- unique(sapply(ans, length))) == 
+            1L) {
+            if (len == 1L) {
+                cl <- lapply(ans, oldClass)
+                cl1 <- cl[[1L]]
+                ans <- unlist(ans, recursive = FALSE)
+                if (!is.null(cl1) && all(sapply(cl, function(x) identical(x, 
+                  cl1)))) 
+                  class(ans) <- cl1
+            }
+            else if (len > 1L) 
+                ans <- matrix(unlist(ans, recursive = FALSE), 
+                  nrow = nry, ncol = len, byrow = TRUE, dimnames = {
+                    if (!is.null(nms <- names(ans[[1L]]))) 
+                      list(NULL, nms)
+                    else NULL
+                  })
+        }
+        ans
+    })
+    len <- length(y)
+    for (i in seq_along(z)) y[[len + i]] <- z[[i]]
+    names(y) <- c(names(by), names(x))
+    row.names(y) <- NULL
+    y
+}
+<bytecode: 0x31ca050>
+<environment: namespace:stats>
