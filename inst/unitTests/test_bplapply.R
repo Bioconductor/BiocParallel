77c068c615a60d4689089e6033705faae5be4cbf
mtmorgan@fhcrc.org <mtmorgan@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-12-23 20:27:58 +0000

    sync unit test with git
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@84994 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_bplapply.R
+++ inst/unitTests/test_bplapply.R
@@ -9,28 +9,48 @@ library(doParallel)                     # FIXME: unload?
             err <<- conditionMessage(e)
         expected
     })
+    if (!is.null(err))
+      print(as.character(err))
     checkTrue(is.null(err))
     checkIdentical(expected, obs)
 }
 
+params <- list(serial=SerialParam(),
+  mc=MulticoreParam(2),
+  snow0=SnowParam(2, "FORK"),
+  snow1=SnowParam(2, "PSOCK"),
+  dopar=DoparParam(),
+  batchjobs=BatchJobsParam())
+
+dop <- registerDoParallel(cores=2)
+## FIXME: restore previously registered back-end?
+
 test_bplapply_Params <- function()
 {
-    params <- list(serial=SerialParam(),
-                   mc=MulticoreParam(2),
-                   snow0=SnowParam(2, "FORK"),
-                   snow1=SnowParam(2, "PSOCK"),
-                   dopar=DoparParam(),
-                   batchjobs=BatchJobsParam(),
-                   dopar=DoparParam())
-
-    dop <- registerDoParallel(cores=2)
-    ## FIXME: restore previously registered back-end?
-
     x <- 1:10
     expected <- lapply(x, sqrt)
     for (ptype in names(params)) {
         .fork_not_windows(expected,
                           bplapply(x, sqrt, BPPARAM=params[[ptype]]))
+    }
+
+
+    # test empty input
+    for (ptype in names(params)) {
+      .fork_not_windows(list(),
+        bplapply(list(), identity, BPPARAM=params[[ptype]]))
+    }
+
+    TRUE
+}
+
+test_bplapply_symbols <- function()
+{
+    X <- list(as.symbol(".XYZ"))
+    expected <- lapply(X, as.character)
+    for (ptype in names(params)) {
+        .fork_not_windows(expected,
+                          bplapply(X, as.character, BPPARAM=params[[ptype]]))
     }
     TRUE
 }
