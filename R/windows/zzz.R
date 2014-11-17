af65efc826dbb315ca28974ae980125a0bf9a38a
v.obenchain <v.obenchain@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-11-17 18:26:57 +0000

    change default BPPARAM to SnowParm() for windows
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@96818 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/windows/zzz.R
+++ R/windows/zzz.R
@@ -18,10 +18,10 @@ setLoadActions(.registerDefaultParams = function(nmspc) {
         ## these fail under complex conditions, e.g., loading a data
         ## set with a class defined in a package that imports
         ## BiocParallel
+        register(getOption("MulticoreParam", MulticoreParam()))
         register(getOption("SerialParam", SerialParam()))
         register(getOption("BatchJobsParam", BatchJobsParam()))
         register(getOption("SnowParam", SnowParam(workers=detectCores())))
-        register(getOption("MulticoreParam", MulticoreParam()))
     }, error=function(err) {
         message("'BiocParallel' did not register default BiocParallelParams")
         NULL
