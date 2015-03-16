aed0aa82004265525afefd4b428a6e65860bfe3e
v.obenchain <v.obenchain@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-03-16 19:23:59 +0000

    modify bpvalidate() to look for symbols in 'fun' environment, NAMESPACE of loaded libraries and search path
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@100729 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bpvalidate.R
+++ R/bpvalidate.R
@@ -1,21 +1,30 @@
-
 bpvalidate <- function(fun)
 {
     if (typeof(fun) != "closure")
         stop("'fun' must be a closure")
-
-    required <- character()
     unknown <- codetools::findGlobals(fun)
+    f_env <- environment(fun)
 
-    ## attempt to load libraries in 'fun'
-    warn <- err <- tryLoad <- NULL
+    ## 'fun' environment is NAMESPACE
+    if (length(unknown) && isNamespace(f_env)) {
+        f_ls <- c(getNamespaceImports(f_env),
+                  setNames(list(ls(f_env, all.names=TRUE)),
+                           getNamespaceName(f_env)))
+        f_symbols <- unique(unlist(f_ls, use.names=FALSE))
+        unknown <- unknown[!unknown %in% f_symbols]
+    }
+ 
+    ## 'fun' body loads libraries
+    warn <- err <- NULL
     if (any(c("require", "library") %in% unknown)) {
-        tryLoad <- withCallingHandlers(tryCatch({
+        xx <- withCallingHandlers(tryCatch({
             dep <- deparse(fun)
             i <- grepl("library", dep, fixed=TRUE) |
                  grepl("require", dep, fixed=TRUE)
-            xx <- sapply(dep[i], function(x) eval(parse(text=x)))
-            NULL
+            sapply(dep[i], function(elt) {
+                pkg <- gsub(")", "", strsplit(elt, "(", fixed=TRUE)[[1]][2])
+                unknown %in% getNamespaceExports(pkg)
+            })
         }, error=function(e) {
             err <<- append(err, conditionMessage(e))
             NULL
@@ -23,44 +32,33 @@ bpvalidate <- function(fun)
             warn <<- append(warn, conditionMessage(w))
             invokeRestart("muffleWarning")
         })
-        if (!is.null(tryLoad)) {
+        if (!is.null(warn) || !is.null(err)) {
             msg <- "attempt to load library failed:\n"
             stop(sprintf(msg, paste(c(warn, err), collapse="\n      ")))
         }
+        unknown <- unknown[rowSums(xx) == 0L]
     }
 
-    ## 'fun' environment is a NAMESPACE
-    if (length(unknown) && isNamespace(environment(fun))) {
-        f_env <- environment(fun)
-        ## add package name
-        required <- c(required, getNamespaceName(f_env))
-        f_ls <- c(getNamespaceImports(f_env),
-                  setNames(list(ls(f_env, all.names=TRUE)),
-                           getNamespaceName(f_env)))
-        f_symbols <- unique(unlist(f_ls, use.names=FALSE))
-        ## filter symbols
-        unknown <- unknown[unknown %in% f_symbols]
-    }
-
-    ## look for symbols on search path
+    ## look in search path
+    inpath <- list()
     if (length(unknown)) {
-        loc <- Map(function(elt) head(find(elt), 1), unknown)
-        ## add package name
-        required <- unique(c(required, sub("package:", "", unlist(loc))))
-        ## filter symbols (.GlobalEnv -> unknown)
-        unknown <- names(Filter(function(elt) length(elt) == 0, loc))
-        if (length(global <- names(loc[loc %in% ".GlobalEnv"])))
-            unknown <- c(unknown, global)
+        inpath <- .foundInPath(unknown)
+        unknown <- setdiff(unknown, names(inpath))
+        inpath <- .filterDefaultPackages(inpath)
     }
-
-    ## filter default packages
-    defaults <- c("stats", "graphics", "grDevices", "utils", "datasets",
-                  "methods", "Autoloads", "base", ".GlobalEnv")
-    required <- required[!required %in% defaults]
-    if (length(required))
-        required <- paste(required, collapse=",")
     if (length(unknown))
         warning("function references unknown symbol(s)")
 
-    list(RequiredPackages=required, UnknownSymbols=unknown)
+    list(inPath=inpath, unknown=unknown)
+}
+
+.foundInPath <- function(symbols) {
+    loc <- Map(function(elt) head(find(elt), 1), symbols)
+    Filter(function(elt) length(elt) != 0, loc)
+}
+
+.filterDefaultPackages <- function(symbols) {
+    defaults <- paste0("package:", c("stats", "graphics", "grDevices", 
+                       "utils", "datasets", "methods", "Autoloads", "base"))
+    Filter(function(elt) !elt %in% defaults, symbols)
 }
