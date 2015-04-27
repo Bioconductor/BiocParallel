87018128dce91b4848e646cfec64d77e2f1a5f7d
Valerie Obenchain <vobencha@fhcrc.org>
2015-04-27 18:51:09 +0000

    add print.remote-error() to tame traceback output
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@103155 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bplapply-methods.R
+++ R/bplapply-methods.R
@@ -1,3 +1,9 @@
+### =========================================================================
+### bplapply methods 
+### -------------------------------------------------------------------------
+
+## All params have dedicated bplapply methods.
+
 setMethod(bplapply, c("ANY", "missing"),
     function(X, FUN, ..., BPPARAM=bpparam())
 {
@@ -9,12 +15,15 @@ setMethod(bplapply, c("ANY", "list"),
     function(X, FUN, ..., BPPARAM=bpparam())
 {
     FUN <- match.fun(FUN)
+
     if (!all(vapply(BPPARAM, is, logical(1), "BiocParallelParam")))
         stop("All elements in 'BPPARAM' must be BiocParallelParam objects")
     if (length(BPPARAM) == 0L)
         stop("'length(BPPARAM)' must be > 0")
-    myFUN <- if (length(BPPARAM) > 1L)
-        function(...) FUN(..., BPPARAM=BPPARAM[-1L])
-    else FUN
+
+    myFUN <- 
+        if (length(BPPARAM) > 1L)
+            function(...) FUN(..., BPPARAM=BPPARAM[-1L])
+        else FUN
     bplapply(X, myFUN, ..., BPPARAM=BPPARAM[[1L]])
 })
--- R/bpmapply-methods.R
+++ R/bpmapply-methods.R
@@ -1,16 +1,22 @@
+### =========================================================================
+### bpmapply methods 
+### -------------------------------------------------------------------------
+
+## BatchJobsParam has a dedicated bpmapply() method. All others dispatch
+## to bplapply() where errors and logging are handled.
+
+
 setMethod(bpmapply, c("ANY", "missing"),
-    function(FUN, ..., MoreArgs=NULL, SIMPLIFY=TRUE, USE.NAMES=TRUE,
-             BPPARAM=bpparam())
+    function(FUN, ..., MoreArgs=NULL, SIMPLIFY=TRUE, 
+             USE.NAMES=TRUE, BPPARAM=bpparam())
 {
     FUN <- match.fun(FUN)
-    bpmapply(FUN, ..., MoreArgs=MoreArgs, SIMPLIFY=SIMPLIFY,
-             USE.NAMES=USE.NAMES, BPPARAM=BPPARAM)
+    bpmapply(FUN, ..., BPPARAM=BPPARAM)
 })
 
-## BatchJobsParam has method for bpmapply, all others dispatch to bplapply.
 setMethod(bpmapply, c("ANY", "BiocParallelParam"),
-    function(FUN, ..., MoreArgs=NULL, SIMPLIFY=TRUE, USE.NAMES=TRUE,
-        BPPARAM=bpparam())
+    function(FUN, ..., MoreArgs=NULL, SIMPLIFY=TRUE, 
+             USE.NAMES=TRUE, BPPARAM=bpparam())
 {
     ## re-package for lapply
     ddd <- .getDotsForMapply(...)
@@ -26,4 +32,23 @@ setMethod(bpmapply, c("ANY", "BiocParallelParam"),
     res <- bplapply(X=seq_along(ddd[[1L]]), wrap, BPPARAM=BPPARAM, 
                     .FUN=FUN, .ddd=ddd, .MoreArgs=MoreArgs)
     .simplify(.rename(res, ddd, USE.NAMES=USE.NAMES), SIMPLIFY)
+})
+
+setMethod(bpmapply, c("ANY", "list"),
+    function(FUN, ..., MoreArgs=NULL, SIMPLIFY=TRUE, 
+             USE.NAMES=TRUE, BPPARAM=bpparam())
+{
+    FUN <- match.fun(FUN)
+
+    if (!all(vapply(BPPARAM, is, logical(1), "BiocParallelParam")))
+        stop("All elements in 'BPPARAM' must be BiocParallelParam objects")
+    if (length(BPPARAM) == 0L)
+        stop("'length(BPPARAM)' must be > 0")
+
+    myFUN <- 
+        if (length(BPPARAM) > 1L)
+            function(...) FUN(..., BPPARAM=BPPARAM[-1L])
+        else FUN
+    bpmapply(myFUN, ..., MoreArgs=MoreArgs, SIMPLIFY=SIMPLIFY,
+             USE.NAMES=TRUE, BPPARAM=BPPARAM[[1L]])
 })
