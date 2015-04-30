6c3ca65513f066909efaab0b894fb60d46bb1816
v.obenchain <v.obenchain@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-04-30 16:22:06 +0000

    work on error handling:
    - all params return try-error and no results when 
      'catch.errors' is FALSE
    - all params use print.remote-error() S3 method
      when errors are returned with results
    - more consistent error handling in .try() and .try_log()
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@103316 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_errorhandling.R
+++ inst/unitTests/test_errorhandling.R
@@ -1,13 +1,11 @@
-checkExceptionText <-
-    function(expr, txt, negate=FALSE, msg="")
+checkExceptionText <- function(expr, txt, negate=FALSE, msg="")
 {
     x <- try(eval(expr), silent=TRUE)
     checkTrue(inherits(x, "try-error"), msg=msg)
     checkTrue(xor(negate, grepl(txt, as.character(x), fixed=TRUE)), msg=msg)
 }
 
-test_errorhandling <-
-    function()
+test_catch.errors <- function()
 {
     # FIXME we need the windows workaround
     library(doParallel)
@@ -17,16 +15,40 @@ test_errorhandling <-
     y <- rev(x)
     f <- function(x, y) if (x > y) stop("whooops") else x + y
 
+    ## catch.errors = FALSE
     params <- list(
         batchjobs=BatchJobsParam(catch.errors=FALSE, progressbar=FALSE),
-        dopar=DoparParam(catch.errors=FALSE))
+        dopar=DoparParam(catch.errors=FALSE),
+        snow=SnowParam(catch.errors=FALSE),
+        multi=MulticoreParam(catch.errors=FALSE),
+        serial=SerialParam(catch.errors=FALSE))
 
     for (param in params) {
         checkExceptionText(bpmapply(f, x, y, BPPARAM=param),
             "Error in FUN(...): whooops", negate=TRUE)
     }
 
-    # check that resume works
+    ## catch.errors = TRUE 
+    params <- list(
+        dopar=DoparParam(),
+        snow=SnowParam(),
+        multi=MulticoreParam(),
+        serial=SerialParam())
+
+    for (param in params) {
+        res <- bplapply(list(1, "2", 3), sqrt, BPPARAM=param)
+        checkTrue(length(res) == 3L)
+        msg <- "non-numeric argument to mathematical function"
+        checkIdentical(conditionMessage(res[[2]]), msg)
+    }
+
+    closeAllConnections()
+    TRUE
+}
+
+test_bpresume <- function()
+{
+    ## BatchJobs only
     x <- 1:10
     y <- rev(x)
     f <- function(x, y) if (x > y) stop("whooops") else x + y
@@ -44,6 +66,27 @@ test_errorhandling <-
 
         res <- bpresume(bpmapply(f.fix, x, y, BPPARAM=param))
         checkIdentical(as.integer(res), c(rep(11L, 5), rep(0L, 5)))
+    }
+
+    closeAllConnections()
+    TRUE
+}
+
+test_log <- function()
+{
+    ## SnowParam, MulticoreParam only
+    params <- list(
+        snow=SnowParam(log=FALSE),
+        snowLog=SnowParam(log=TRUE),
+        multi=MulticoreParam(log=FALSE),
+        multiLog=MulticoreParam(log=TRUE))
+
+    for (param in params) {
+        res <- bplapply(list(1, "2", 3), sqrt, BPPARAM=param)
+        checkTrue(length(res) == 3L)
+        msg <- "non-numeric argument to mathematical function"
+        checkIdentical(conditionMessage(res[[2]]), msg)
+        checkTrue(length(attr(res[[2]], "traceback")) > 0L)
     }
 
     closeAllConnections()
