836596734b8a766cad283762da4369fdef5ccbf2
Valerie Obenchain <vobencha@fhcrc.org>
2015-05-13 21:08:36 +0000

    add vignette for error handling and logs
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@103789 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/test_logging.R
@@ -0,0 +1,23 @@
+## Test 'log' and 'progressbar'. See test_errorhandling.R for
+## tests with 'stop.on.error' and 'catch.errors'.
+
+test_log <- function()
+{
+    ## SnowParam, MulticoreParam only
+    params <- list(
+        snow=SnowParam(log=FALSE),
+        snowLog=SnowParam(log=TRUE),
+        multi=MulticoreParam(log=FALSE),
+        multiLog=MulticoreParam(log=TRUE))
+
+    for (p in params) {
+        res <- bplapply(list(1, "2", 3), sqrt, BPPARAM=p)
+        checkTrue(length(res) == 3L)
+        msg <- "non-numeric argument to mathematical function"
+        checkIdentical(conditionMessage(res[[2]]), msg)
+        checkTrue(length(attr(res[[2]], "traceback")) > 0L)
+    }
+
+    closeAllConnections()
+    TRUE
+}
