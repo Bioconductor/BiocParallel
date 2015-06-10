702a89d651f381abc39695a176042171289ea74e
v.obenchain <v.obenchain@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-06-10 20:29:07 +0000

    fix bug in bplapply() and bpmapply() methods when BPPARAM is list
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@104804 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bplapply-methods.R
+++ R/bplapply-methods.R
@@ -22,8 +22,11 @@ setMethod(bplapply, c("ANY", "list"),
         stop("'length(BPPARAM)' must be > 0")
 
     myFUN <- 
-        if (length(BPPARAM) > 1L)
-            function(...) FUN(..., BPPARAM=BPPARAM[-1L])
-        else FUN
-    bplapply(X, myFUN, ..., BPREDO=BPREDO, BPPARAM=BPPARAM[[1L]])
+        if (length(BPPARAM) > 1L) {
+          if (length(param <- BPPARAM[-1]) == 1L)
+            function(...) FUN(..., BPPARAM=param[[1]])
+          else
+            function(...) FUN(..., BPPARAM=param)
+        } else FUN
+    bplapply(X, myFUN, ..., BPREDO=BPREDO, BPPARAM=BPPARAM[[1]])
 })
