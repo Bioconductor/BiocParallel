7ad46a3f8a0398d32acf76c01d51787a5db91fe4
v.obenchain <v.obenchain@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-07-29 18:25:40 +0000

    register SerialParam() on machines with <= 3 cores
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@106884 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/unix/zzz.R
+++ R/unix/zzz.R
@@ -36,9 +36,11 @@ setLoadActions(.registerDefaultParams = function(nmspc) {
         ## these fail under complex conditions, e.g., loading a data
         ## set with a class defined in a package that imports
         ## BiocParallel
-        register(getOption("MulticoreParam", MulticoreParam()), TRUE)
-        register(getOption("SnowParam", SnowParam()), FALSE)
-        register(getOption("SerialParam", SerialParam()), FALSE)
+        if ((parallel::detectCores() - 2L) > 1L) {
+            register(getOption("MulticoreParam", MulticoreParam()), TRUE)
+            register(getOption("SnowParam", SnowParam()), FALSE)
+            register(getOption("SerialParam", SerialParam()), FALSE)
+        } else register(getOption("SerialParam", SerialParam()), TRUE)
     }, error=function(err) {
         message("'BiocParallel' did not register default ",
                 "BiocParallelParams:\n  ", conditionMessage(err))
