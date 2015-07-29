69344c1a99457d09b7a7bf9a2b8f71fbebfd4b15
v.obenchain <v.obenchain@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-07-29 16:59:10 +0000

    add check for 0 cores in snowWorkers(), multicoreWorkers()
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@106879 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/SnowParam-class.R
+++ R/SnowParam-class.R
@@ -14,7 +14,12 @@ makeCluster <- parallel::makeCluster
 ###
 
 snowWorkers <- function() {
-    cores <- min(8L, parallel::detectCores() - 2)
+    cores <- min(8L, parallel::detectCores() - 2L)
+    if (cores <= 1L) {
+        cores <- 1L
+        warning(paste0("using a single core; to increase cores specify ",
+                       "'workers' e.g., SnowParam(workers = 2) "))
+    }
     getOption("mc.cores", cores)
 }
 
