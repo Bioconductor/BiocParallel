d3591d9d817e66f13bfe25b3969ada6187223c00
Valerie Obenchain <vobencha@fhcrc.org>
2015-07-29 18:25:40 +0000

    register SerialParam() on machines with <= 3 cores
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@106884 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/MulticoreParam-class.R
+++ R/MulticoreParam-class.R
@@ -7,14 +7,7 @@ multicoreWorkers <- function() {
         if (.Platform$OS.type == "windows") {
             1L
         } else {
-            cores <- min(8L, parallel::detectCores() - 2L)
-            if (cores <= 1L) {
-                cores <- 1L
-                warning(paste0("using a single core; to increase cores ",
-                               "specify 'workers' e.g., ",
-                               "MulticoreParam(workers = 2) "))
-            }
-            cores
+            max(1L, parallel::detectCores() - 2L)
         }
     getOption("mc.cores", cores)
 }
