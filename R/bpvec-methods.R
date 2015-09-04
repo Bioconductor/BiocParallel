9b53eb1da18ed63c9acdd8b10e3206b8a9bc5fab
Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
2015-09-04 14:35:01 -0700

    Commit made by the Bioconductor Git-SVN bridge.
    Consists of 1 commit.
    
    Commit information:
    
    SVN Revision number: 108205
    Commit message:
    fix bug in bpvec() related to splitting X
    
    Committed by v.obenchain
    Committed at: 2015-09-04T21:20:58.726621Z

--- R/bpvec-methods.R
+++ R/bpvec-methods.R
@@ -22,9 +22,16 @@ setMethod(bpvec, c("ANY", "BiocParallelParam"),
 
     if (is.na(bpworkers(BPPARAM)))
         stop("'bpworkers' must be set in your backend to use bpvec")
-
-    si <- .splitX(seq_along(X), bpworkers(BPPARAM), bptasks(BPPARAM))
-    ans <- bplapply(si, FUN, ..., BPREDO=BPREDO, BPPARAM=BPPARAM) 
+    ## pvec parellelizes execution of a function on vector elements 
+    ## by splitting the vector and submitting each part to one core.
+    ## This is equivalent to saying length(X) is evenly divided over
+    ## available cores which is the (default) behavior of bplapply()
+    ## for SnowParam and MulticoreParam when tasks = 0.
+    if (is(BPPARAM, "DoparParam") || is(BPPARAM, "BatchJobsParam"))
+        X <- .splitX(X, bpworkers(BPPARAM), 0)
+    else if (bptasks(BPPARAM) != 0)
+        bptasks(BPPARAM) <- 0
+    ans <- bplapply(X, FUN, ..., BPREDO=BPREDO, BPPARAM=BPPARAM) 
     do.call(AGGREGATE, ans)
 })
 
