6cc60683f3727867faf43188ee39e2c7b09dec54
Valerie Obenchain <vobencha@fhcrc.org>
2015-10-07 20:32:50 +0000

    fix bug in bpvec,ANY,MulticoreParam-method related to '[[' extraction
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@109324 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bpvec-methods.R
+++ R/bpvec-methods.R
@@ -14,19 +14,19 @@ setMethod(bpvec, c("ANY", "missing"),
     bpvec(X, FUN, ..., AGGREGATE=AGGREGATE, BPREDO=BPREDO, BPPARAM=BPPARAM)
 })
 
+## FIXME: bpvec should assume FUN is vectorized and apply FUN() to a
+##        group of X elements, not individual elements. Currently
+##        bplapply (Snow, Multicore) lapply's over the elements. bpvec 
+##        should have its own implementation where the 'argfun' argument to 
+##        .bpdynamicClusterApply is not lapply().
 setMethod(bpvec, c("ANY", "BiocParallelParam"),
     function(X, FUN, ..., AGGREGATE=c, BPREDO=list(), BPPARAM=bpparam())
 {
     FUN <- match.fun(FUN)
     AGGREGATE <- match.fun(AGGREGATE)
-
     if (is.na(bpworkers(BPPARAM)))
         stop("'bpworkers' must be set in your backend to use bpvec")
-    ## pvec parellelizes execution of a function on vector elements 
-    ## by splitting the vector and submitting each part to one core.
-    ## This is equivalent to saying length(X) is evenly divided over
-    ## available cores which is the (default) behavior of bplapply()
-    ## for SnowParam and MulticoreParam when tasks = 0.
+    ## tasks = 0 is even split of X over workers
     if (is(BPPARAM, "DoparParam") || is(BPPARAM, "BatchJobsParam"))
         X <- .splitX(X, bpworkers(BPPARAM), 0)
     else if (bptasks(BPPARAM) != 0)
