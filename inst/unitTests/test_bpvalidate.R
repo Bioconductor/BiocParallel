ac5dc4960391757fd43dcb53560cbd1003d3b0c5
mtmorgan@fhcrc.org <mtmorgan@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-12-28 19:19:46 +0000

    cleanup: don't use Rsamtools in unit tests
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocParallel@111945 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_bpvalidate.R
+++ inst/unitTests/test_bpvalidate.R
@@ -1,34 +1,66 @@
-test_bpvalidate_library <- function()
+test_bpvalidate_basic_ok <- function()
 {
-    .test_env <- new.env(parent=emptyenv())
-    .test_env$countBam <- function(...) {}
-    attach(.test_env)
-    on.exit(detach(.test_env))
-    fun <- function(fl, ...) {
-        countBam(fl)
-    }
-    res <- suppressWarnings(bpvalidate(fun))
-    checkIdentical(names(res$inPath), "countBam")
-    checkIdentical(res$unknown, character())
+    target <- list(inPath=structure(list(), names=character()),
+                   unknown=character())
+    checkIdentical(target, bpvalidate(function() {}         ))
+    checkIdentical(target, bpvalidate(function(x) x         ))
+    checkIdentical(target, bpvalidate(function(x) x()       ))
+    checkIdentical(target, bpvalidate(function(..., x) x    ))
+    checkIdentical(target, bpvalidate(function(..., x) x()  ))
+    checkIdentical(target, bpvalidate(function(y, x) y(x)   ))
+    checkIdentical(target, bpvalidate(function(y, x) y(x=x) ))
+    checkIdentical(target, bpvalidate(function(y, ...) y(...) ))
 }
 
-test_bpvalidate_args <- function()
+test_bpvalidate_basic_fail <- function()
 {
-    param <- Rsamtools::ScanBamParam(
-        flag=Rsamtools::scanBamFlag(isMinusStrand=FALSE))
-    fun <- function(fl, ...) {
-        library(Rsamtools) 
-        countBam(fl, param=param)
-    }
-    res <- suppressWarnings(bpvalidate(fun))
-    checkIdentical(unname(res$inPath), list())
-    checkIdentical(res$unknown, "param") ## no .GlobalEnv; param -> unknown
+    target <- list(inPath=structure(list(), names=character()), unknown="x")
 
-    fun <- function(fl, ..., param) {
-        library(Rsamtools) 
-        countBam(fl, param=param)
-    }
-    res <- bpvalidate(fun)
-    checkIdentical(unname(res$inPath), list())
-    checkIdentical(res$unknown, character())
+    suppressWarnings({
+        checkIdentical(target, bpvalidate(function() x            ))
+        checkIdentical(target, bpvalidate(function() x()          ))
+        checkIdentical(target, bpvalidate(function(y) x + y       ))
+        checkIdentical(target, bpvalidate(function(y) y(x)        ))
+        checkIdentical(target, bpvalidate(function(y) y(x=x)      ))
+        checkIdentical(target, bpvalidate(function(y, ...) y(x)   ))
+        checkIdentical(target, bpvalidate(function(y, ...) y(x=x) ))
+    })
+}
+
+test_bpvalidate_search_path <- function()
+{
+    target <- list(inPath=list(x=".test_env"), unknown=character())
+
+    .test_env <- new.env(parent=emptyenv())
+    .test_env$x <- NULL
+    attach(.test_env)
+    on.exit(detach(.test_env))
+
+    checkIdentical(target, bpvalidate(function() x            ))
+    checkIdentical(target, bpvalidate(function(...) x         ))
+    checkIdentical(target, bpvalidate(function(y, ...) y(x)   ))
+    checkIdentical(target, bpvalidate(function(y, ...) y(x=x) ))
+
+    ## FIXME: should fail -- in search(), but not a function!
+    ## checkIdentical(target, bpvalidate(function() x() ))
+}
+
+test_bpvalidate_library <- function()
+{
+    target <- list(inPath=structure(list(), names=character()),
+                   unknown=character())
+
+    checkException(bpvalidate(function() library("__UNKNOWN__")), silent=TRUE)
+    checkException(bpvalidate(function() require("__UNKNOWN__")), silent=TRUE)
+
+    ## FIXME: internally, bpvalidate code expects a matrix but gets a vector
+    ## bpvalidate(function() library(BiocParallel))
+    ## FIXME: bpvalidate expects unquoted arg to library() / require()
+    ## bpvalidate(function() library("BiocParallel"))
+
+    target1 <- list(inPath=list(bpvalidate = "package:BiocParallel"),
+                    unknown = character(0))
+    checkIdentical(target1, bpvalidate(function() bpvalidate())) # inPath
+    fun <- function() { library(BiocParallel); bpvalidate() }
+    checkIdentical(target, bpvalidate(fun))                      # in function
 }
