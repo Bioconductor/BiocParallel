0aeb23d77aaab37a19688d42e48cfc3124bede64
Nitesh Turaga <nitesh.turaga@gmail.com>
2017-10-11 18:30:45 -0400

    remove keys

--- inst/extdata/demo.R
+++ inst/extdata/demo.R
@@ -8,8 +8,6 @@ image <-  "ami-9fe2fee4"
 sg <- "sg-748dcd07"
 subnet <- "subnet-d66a05ec"
 
-access_key = "AKIAISE2U2VGKIRCWQTA"
-secret_key = "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"
 
 #access_key = strsplit(readLines("~/.aws/credentials"), " = ")[[2]][[2]]
 #secret_key = strsplit(readLines("~/.aws/credentials"), " = ")[[3]][[2]]
@@ -55,3 +53,28 @@ cl <- snow::makeSOCKcluster(
 )
 
 
+param = SnowParam(
+    ips,
+    rshcmd = "ssh -i ~/.ssh/bioc-default.pem -v",
+    user="ubuntu",
+    rhome="/usr/local/lib/R",
+    snowlib = "/home/ubuntu/R/x86_64-pc-linux-gnu-library/3.4",
+    rscript = "/usr/local/bin/Rscript",
+    outfile = "/home/ubuntu/snow.log",
+    master = aws.ec2::my_ip()
+)
+
+## stop the instance
+ e = environment(BiocParallel:::.awsCluster$set)
+ e[["cl"]]= NULL
+
+## Make new security group called "testing aws param"
+
+## start a snow param
+bplapply(1:4, function(i) system("hostname", intern=TRUE), BPPARAM=param)
+bpstop(param)
+
+
+## snow
+
+ snow::parLapply(cl, 1:5, function(i) system("hostname"))
