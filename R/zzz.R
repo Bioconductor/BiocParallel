7e1dd7e077b98ed3e5a323536582d40833416d52
Martin Morgan <martin.morgan@roswellpark.org>
2019-01-07 20:27:58 -0500

    don't advance random number seed on package load
    - see https://stat.ethz.ch/pipermail/bioc-devel/2019-January/014535.html
    - Thanks, Henrik Bengtsson

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,12 @@
     ## initial port assigned on load, rather than on first use
     ## provides consistency in random number seed advance, see
     ## https://stat.ethz.ch/pipermail/bioc-devel/2019-January/014526.html
+    ##
+    ## don't perturb the random number stream, see
+    ## https://stat.ethz.ch/pipermail/bioc-devel/2019-January/014535.html
+    oseed <- .GlobalEnv$.Random.seed
+    on.exit(.GlobalEnv$.Random.seed <- oseed)
+
     port <- new.env(parent = emptyenv(), size = 3L)
     port[["SnowParam"]] <- .snowPort()
     port[["MulticoreParam"]] <- .snowPort()
