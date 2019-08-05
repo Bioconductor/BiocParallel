85516c916cc2b0a7bc53527c096068128a3ce205
Martin Morgan <martin.morgan@roswellpark.org>
2019-08-05 14:58:06 -0400

    mcparallel() / mccollect() impl of faster MulticoreParam

---
+++ R/TransientMulticoreParam-class.R
@@ -0,0 +1,118 @@
+.TransientMulticoreParam <-
+    setRefClass(
+        "TransientMulticoreParam",
+        contains = "MulticoreParam"
+    )
+
+TransientMulticoreParam <-
+    function(param)
+{
+    as(param, "TransientMulticoreParam")
+}
+
+.TRANSIENTMULTICOREPARAM_JOBNODE <- new.env(parent=emptyenv())
+.TRANSIENTMULTICOREPARAM_RESULT <- new.env(parent=emptyenv())
+
+setMethod(
+    "bpstart", "TransientMulticoreParam",
+    function(x, ...)
+{
+    mccollect(wait=TRUE)
+    rm(
+        list=ls(envir = .TRANSIENTMULTICOREPARAM_JOBNODE),
+        envir = .TRANSIENTMULTICOREPARAM_JOBNODE
+    )
+    rm(
+        list = ls(envir = .TRANSIENTMULTICOREPARAM_RESULT),
+        envir = .TRANSIENTMULTICOREPARAM_RESULT
+    )
+    .bpstart_impl(x)
+})
+
+setMethod(
+    "bpstop", "TransientMulticoreParam",
+    function(x)
+{
+    .bpstop_impl(x)
+})
+
+setMethod(
+    "bpbackend", "TransientMulticoreParam",
+    function(x)
+{
+    x
+})
+
+setMethod(
+    "length", "TransientMulticoreParam",
+    function(x)
+{
+    bpnworkers(x)
+})
+
+##
+## send / recv
+##
+
+setMethod(
+    ".recv_all", "TransientMulticoreParam",
+    function(backend)
+{
+    replicate(length(backend), .recv_any(backend), simplify=FALSE)
+})
+
+setMethod(
+    ".send_to", "TransientMulticoreParam",
+    function(backend, node, value)
+{
+    if (value$type == "EXEC") {
+        job <- mcparallel(.bpworker_EXEC(value))
+        id <- as.character(job$pid)
+        .TRANSIENTMULTICOREPARAM_JOBNODE[[id]] <- node
+    }
+    TRUE
+})
+
+setMethod(
+    ".recv_any", "TransientMulticoreParam",
+    function(backend)
+{
+    .BUFF <- .TRANSIENTMULTICOREPARAM_RESULT # alias
+    tryCatch({
+        while (!length(.BUFF)) {
+            result <- mccollect(wait = FALSE, timeout = 1)
+            for (id in names(result))
+                .BUFF[[id]] <- result[[id]]
+        }
+        id <- sample(names(.BUFF), 1L)
+        value <- .BUFF[[id]]
+        rm(list = id, envir = .BUFF)
+        node <- .TRANSIENTMULTICOREPARAM_JOBNODE[[id]]
+        rm(list = id, envir = .TRANSIENTMULTICOREPARAM_JOBNODE)
+        list(node = node, value = value)
+    }, error  = function(e) {
+        ## indicate error, but do not stop
+        .error_worker_comm(e, "'.recv_any()' data failed")
+    })
+})
+
+setMethod(
+    ".send", "TransientMulticoreParam",
+    function(worker, value)
+{
+    stop("'.send,TransientMulticoreParam-method' not implemented")
+})
+
+setMethod(
+    ".recv", "TransientMulticoreParam",
+    function(worker)
+{
+    stop("'.recv,TransientMulticoreParam-method' not implemented")
+})
+
+setMethod(
+    ".close", "TransientMulticoreParam",
+    function(worker)
+{
+    stop("'.close,TransientMulticoreParam-method' not implemented")
+})
