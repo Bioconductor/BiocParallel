c3287dd30a6430acd401e4f895a01a7a4a9ff043
Jiefei-Wang <szwjf08@gmail.com>
2022-04-27 11:22:32 -0400

    update C++ code using cpp11 to improve error handling

--- inst/unitTests/test_ipcmutex.R
+++ inst/unitTests/test_ipcmutex.R
@@ -23,3 +23,24 @@ test_ipccounter <- function()
     }, id, BPPARAM=SnowParam(2))
     checkIdentical(sort(unlist(result, use.names=FALSE)), 1:5)
 }
+
+test_ipc_errors <- function()
+{
+    ## Error : Expected 'character' actual 'double'
+    checkException(ipclock(123))
+    ## Error : 'id' must be character(1) and not NA
+    checkException(ipclock(NA_character_))
+    ## Error : 'id' must be character(1) and not NA
+    checkException(ipclock(letters))
+
+    ## expect no error
+    id <- ipcid()
+    ipcreset(id, 10)
+
+    ## Error: Expected single integer value
+    checkException(ipcreset(id, 1:3))
+    ## Error: 'n' must not be NA
+    checkException(ipcreset(id, NA_integer_))
+
+    ipcremove(id)
+}
