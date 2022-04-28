9d7d0fb0b9491a528e303a357e7830e8f412d219
Jiefei-Wang <szwjf08@gmail.com>
2022-04-28 10:34:26 -0400

    Deprecate BatchJobsParam

--- R/BatchJobsParam-class.R
+++ R/BatchJobsParam-class.R
@@ -41,6 +41,7 @@ BatchJobsParam <-
         conf.pars=list(conffile=conffile, cluster.functions=cluster.functions),
         submit.pars=list(resources=resources), ...)
 {
+    .Deprecated("BatchToolsParam")
     if (!requireNamespace("BatchJobs", quietly=TRUE))
         stop("BatchJobsParam() requires the 'BatchJobs' package")
 
