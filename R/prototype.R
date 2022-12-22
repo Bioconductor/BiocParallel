2f379a9eb4fb2a5c37923404b5b45d0f7c4ff286
Martin Morgan <Martin.Morgan@RoswellPark.org>
2022-12-22 11:41:07 -0500

    distinguish between 'connect' and 'idle' timeout
    - connect timeout from getOptions("timeout") (as previously)
    - idle timeout (no activity on socket) fixed at 30 days, as in snow /
      parallel (new)
    - compute timeout from timeout= argument to *Param() (as previously)

--- R/prototype.R
+++ R/prototype.R
@@ -1,5 +1,20 @@
+## There are three timeouts involved
+##
+## - establishing a socket connection, from getOption("timeout"),
+##   default 60 seconds
+## - duration of allowed computations, from argument `timeout=` to
+##   *Param(), default WORKER_TIMEOUT (infinite)
+## - duration of idle connections (no activity from the worker
+##   socket), default IDLE_TIMEOUT (30 days) beause (a) this is the
+##   snow behavior and (b) sockets appear to sometimes segfault & lead
+##   to PROTECTion imbalance if an attempt is made to write to a
+##   terminated socket.
+
 ## Timeout for individual worker tasks
 WORKER_TIMEOUT <- NA_integer_
+
+## Timeout for socket inactivity
+IDLE_TIMEOUT <- 2592000L # 60 * 60 * 24 * 30 = 30 day; consistent w/ parallel
 
 ## Maximum number of tasks, e.g., when using progress bar
 TASKS_MAXIMUM <- .Machine$integer.max
